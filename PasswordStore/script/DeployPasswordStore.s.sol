6e3c9caf438d0d3dbac40ee77320fb6c626a0783
jonas-sebera <sebejaz99@gmail.com>
2023-10-19 15:19:08 +0200

    turckling deploy tests

--- PasswordStore/script/DeployPasswordStore.s.sol
+++ PasswordStore/script/DeployPasswordStore.s.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Script, console2} from "forge-std/Script.sol";
-import {PasswordStore} from "../src/PasswordStore.sol";
+import { Script, console2 } from "forge-std/Script.sol";
+import { PasswordStore } from "../src/PasswordStore.sol";
 
 contract DeployPasswordStore is Script {
     function run() public returns (PasswordStore) {
@@ -14,16 +14,3 @@ contract DeployPasswordStore is Script {
     }
 }
 
-import { Script, console2 } from "forge-std/Script.sol";
-import { PasswordStore } from "../src/PasswordStore.sol";
-
-contract DeployPasswordStore is Script {
-    function run() public returns(PasswordStore) {
-        vm.startBroadcast();
-        PasswordStore passwordStore = new PasswordStore();
-        PasswordStore.setPassword("myPassword");
-        vm.stopBroadcast();
-
-        return passwordStore;
-    }
-}
---
+++ PasswordStore/test/DeployPasswordStore.t.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import { Test, console } from "forge-std/Test.sol";
+import { DeployPasswordStore } from "../script/DeployPasswordStore.s.sol";
+import {}
